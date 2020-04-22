5283f450d76b067bbabd178eb6927e6fc761d9da
rajkush211 <rajkush211.rk@gmail.com>
2020-04-23 02:19:42 +0530

    ADDED : Test case when given 0 feet and null value should return not equal

--- src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -11,4 +11,11 @@ public class QuantityMeasurementTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void given0FeetAndNullValue_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = null;
+        Assert.assertNotEquals(feet1, feet2);
+    }
 }
