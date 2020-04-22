1b155bfab533eb0e39bff6eb0e5e28b0bcbf0843
rajkush211 <rajkush211.rk@gmail.com>
2020-04-23 02:11:35 +0530

    ADDED : Test and code to return equal when given 0 feet and 0 feet

---
+++ src/test/java/com/bridgelabz/quantitymeasurement/QuantityMeasurementTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.quantitymeasurement;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuantityMeasurementTest {
+
+    @Test
+    public void given0FeetAnd0Feet_ShouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(0.0);
+        Assert.assertEquals(feet1, feet2);
+    }
+}
